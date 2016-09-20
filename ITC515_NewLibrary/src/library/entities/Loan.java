9a972d5de7cbc2e3444a84d05339ab2acd65d820
Andrew Perera <andrew5core@hotmail.com>
2016-09-20 11:23:09 +1000

    Variables added as standard

--- ITC515_NewLibrary/src/library/entities/Loan.java
+++ ITC515_NewLibrary/src/library/entities/Loan.java
@@ -10,22 +10,22 @@ import library.interfaces.entities.ELoanState;
 
 public class Loan implements ILoan {
 
-	private int id;
-	private final IMember borrower;
-	private final IBook book;
-	private Date borrowDate;
-	private Date dueDate;
-	private ELoanState state;
+	private int id_;
+	private final IMember borrower_;
+	private final IBook book_;
+	private Date borrowDate_;
+	private Date dueDate_;
+	private ELoanState state_;
 	
 	public Loan(IBook book, IMember borrower, Date borrowDate, Date returnDate) {
 		if (!sane(book, borrower, borrowDate, returnDate)) {
 			throw new IllegalArgumentException("Loan: constructor : bad parameters");
 		}
-		this.book = book;
-		this.borrower = borrower;
-		this.borrowDate = borrowDate;
-		this.dueDate = returnDate;	
-		this.state = ELoanState.PENDING;
+		this.book_ = book;
+		this.borrower_ = borrower;
+		this.borrowDate_ = borrowDate;
+		this.dueDate_ = returnDate;	
+		this.state_ = ELoanState.PENDING;
 	}
 	
 	private boolean sane(IBook book, IMember borrower, Date borrowDate, Date returnDate) {
@@ -38,75 +38,75 @@ public class Loan implements ILoan {
 
 	@Override
 	public void commit(int loanId) {
-		if (!(state == ELoanState.PENDING)) {
+		if (!(state_ == ELoanState.PENDING)) {
 			throw new RuntimeException(
 					String.format("Loan : commit : incorrect state transition  : %s -> %s\n", 
-							state, ELoanState.CURRENT));
+							state_, ELoanState.CURRENT));
 		}
 		if (loanId <= 0) {
 			throw new RuntimeException(
 					String.format("Loan : commit : id must be a positive integer  : %d\n", 
 							loanId));
 		}
-		this.id = loanId;
-		state = ELoanState.CURRENT;
-		book.borrow(this);
-		borrower.addLoan(this);
+		this.id_ = loanId;
+		state_ = ELoanState.CURRENT;
+		book_.borrow(this);
+		borrower_.addLoan(this);
 	}
 
 	@Override
 	public void complete() {
-		if (!(state == ELoanState.CURRENT || state == ELoanState.OVERDUE)) {
+		if (!(state_ == ELoanState.CURRENT || state_ == ELoanState.OVERDUE)) {
 			throw new RuntimeException(
 					String.format("Loan : complete : incorrect state transition  : %s -> %s\n",
-							state, ELoanState.COMPLETE));
+							state_, ELoanState.COMPLETE));
 		}
-		state = ELoanState.COMPLETE;		
+		state_ = ELoanState.COMPLETE;		
 	}
 
 	@Override
 	public boolean isOverDue() {
-		return (state == ELoanState.OVERDUE);
+		return (state_ == ELoanState.OVERDUE);
 	}
 
 	@Override
 	public boolean checkOverDue(Date currentDate) {
-		if (!(state == ELoanState.CURRENT || state == ELoanState.OVERDUE )) {
+		if (!(state_ == ELoanState.CURRENT || state_ == ELoanState.OVERDUE )) {
 			throw new RuntimeException(
 					String.format("Loan : checkOverDue : incorrect state transition  : %s -> %s\n",
-							state, ELoanState.OVERDUE));
+							state_, ELoanState.OVERDUE));
 		}
-		if (currentDate.compareTo(dueDate) > 0) {
-			state = ELoanState.OVERDUE;
+		if (currentDate.compareTo(dueDate_) > 0) {
+			state_ = ELoanState.OVERDUE;
 		}
 		return isOverDue();
 	}
 
 	@Override
 	public IMember getBorrower() {
-		return borrower;
+		return borrower_;
 	}
 
 	@Override
 	public IBook getBook() {
-		return book;
+		return book_;
 	}
 
 	@Override
 	public int getID() {
-		return id;
+		return id_;
 	}
 	
 	public ELoanState getState() {
-		return state;
+		return state_;
 	}
 
 	@Override
 	public String toString() {
 		return (String.format("Loan ID:  %d\nAuthor:   %s\nTitle:    %s\nBorrower: %s %s\nBorrowed: %s\nDue Date: %s", 
-				id, book.getAuthor(), book.getTitle(), borrower.getFirstName(), borrower.getLastName(),
-				DateFormat.getDateInstance().format(borrowDate),
-				DateFormat.getDateInstance().format(dueDate)));
+				id_, book_.getAuthor(), book_.getTitle(), borrower_.getFirstName(), borrower_.getLastName(),
+				DateFormat.getDateInstance().format(borrowDate_),
+				DateFormat.getDateInstance().format(dueDate_)));
 	}
 
 
