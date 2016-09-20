9e71733fb3a44d3a1c0f69343360f7dccf432ada
Kishantha <kishantha@gmail.com>
2016-09-20 19:50:31 +1000

    Get member by id test case added

--- ITC515_NewLibrary/src/test/library/daos/MemberMapDAOTest.java
+++ ITC515_NewLibrary/src/test/library/daos/MemberMapDAOTest.java
@@ -71,7 +71,34 @@ public class MemberMapDAOTest {
 		
 	}
 
+	@Test
+	public void testGetMemberById(){
 
-	
-	
+		
+		IMemberHelper helper = Mockito.mock(IMemberHelper.class);
+		IMember iMember = Mockito.mock(IMember.class);
+		MemberMapDAO memberMapDAO = new MemberMapDAO(helper);
+		
+		Mockito.when(helper.makeMember(MEMBER_01_FIRST_NAME, MEMBER_01_LAST_NAME, MEMBER_01_CONTACTPHONE_NUMBER, MEMBER_01_CONTACT_EMAIL, MEMBER_01_ID)).thenReturn(iMember);
+		Mockito.when(iMember.getID()).thenReturn(MEMBER_01_ID);
+		Mockito.when(iMember.getFirstName()).thenReturn(MEMBER_01_FIRST_NAME);
+		Mockito.when(iMember.getLastName()).thenReturn(MEMBER_01_LAST_NAME);
+		Mockito.when(iMember.getContactPhone()).thenReturn(MEMBER_01_CONTACTPHONE_NUMBER);
+		Mockito.when(iMember.getEmailAddress()).thenReturn(MEMBER_01_CONTACT_EMAIL);
+		
+		//Add one member fist
+		memberMapDAO.addMember(MEMBER_01_FIRST_NAME, MEMBER_01_LAST_NAME, MEMBER_01_CONTACTPHONE_NUMBER, MEMBER_01_CONTACT_EMAIL);
+		
+		//Lets try to get that member
+		IMember member = memberMapDAO.getMemberByID(MEMBER_01_ID);
+		
+		//Lets check the members properties
+		assertEquals(MEMBER_01_ID, member.getID());
+		assertEquals(MEMBER_01_FIRST_NAME, member.getFirstName());
+		assertEquals(MEMBER_01_LAST_NAME, member.getLastName());
+		assertEquals(MEMBER_01_CONTACTPHONE_NUMBER, member.getContactPhone());
+		assertEquals(MEMBER_01_CONTACT_EMAIL, member.getEmailAddress());
+		
+	}
+
 }
