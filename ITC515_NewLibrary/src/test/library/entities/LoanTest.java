fc933480104d3dbc19604e6b0a10865055078b5a
Andrew Perera <andrew5core@hotmail.com>
2016-09-21 00:37:19 +1000

    Libraries imported and classes extended TestCase

--- ITC515_NewLibrary/src/test/library/entities/LoanTest.java
+++ ITC515_NewLibrary/src/test/library/entities/LoanTest.java
@@ -1,5 +1,23 @@
 package test.library.entities;
 
-public class LoanTest {
+import junit.framework.TestCase;
+
+
+import library.interfaces.entities.ELoanState;
+import library.interfaces.entities.IBook;
+import library.interfaces.entities.ILoan;
+import library.interfaces.entities.IMember;
+import library.entities.Loan;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+import org.junit.Test;
+
+import org.mockito.Mockito;
+
+
+public class LoanTest extends TestCase{
 
 }
