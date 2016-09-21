ffedd320332d3888089afd3a3a90692b6f507ac2
Kishantha <kishantha@gmail.com>
2016-09-21 21:17:33 +1000

    Test case for getLoan method

--- ITC515_NewLibrary/src/test/library/entities/BookLowLevelIntegrationTest.java
+++ ITC515_NewLibrary/src/test/library/entities/BookLowLevelIntegrationTest.java
@@ -55,44 +55,60 @@ public class BookLowLevelIntegrationTest {
 	}
 	
 	@Test
-	public void testBorrowerHasReachedLoanLimit(){
+	public void testBorrowBookMoreThanOnce(){
+		
+		IBookDAO bookDAO = new BookMapDAO(new BookHelper());
+		ILoanDAO loanDAO = new LoanMapDAO(new LoanHelper());
+		IMemberDAO memberDAO = new MemberMapDAO(new MemberHelper());
+		IMember[] member = new IMember[2];
+		
+		member[0] = memberDAO.addMember("fName0", "lName0", "0001", "email0");
+		member[1] = memberDAO.addMember("fName1", "lName1", "0002", "email1");
+		
+		IBook book  = bookDAO.addBook("author1", "title1", "callNo1");
+
+		
+		try{
+			for (int i=0; i<2; i++) {
+				ILoan loan = loanDAO.createLoan(member[i], book);
+				loanDAO.commitLoan(loan);
+			}
+		
+			fail("Cannot borrow same book more than once");
+		}catch(RuntimeException ignoredException){
+			//ignore as this is the default behaviour
+		}
+		
+		assertEquals(EBookState.ON_LOAN,book.getState());
+
+		
+	}
+	
+
+	
+	@Test
+	public void testGetLoan(){
 		
 		IBookDAO bookDAO = new BookMapDAO(new BookHelper());
 		ILoanDAO loanDAO = new LoanMapDAO(new LoanHelper());
 		IMemberDAO memberDAO = new MemberMapDAO(new MemberHelper());
 		
-		IBook book[] = new IBook[15];
-		
-		book[0]  = bookDAO.addBook("author1", "title1", "callNo1");
-		book[1]  = bookDAO.addBook("author1", "title2", "callNo2");
-		book[2]  = bookDAO.addBook("author1", "title3", "callNo3");
-		book[3]  = bookDAO.addBook("author1", "title4", "callNo4");
-		book[4]  = bookDAO.addBook("author2", "title5", "callNo5");
-		book[5]  = bookDAO.addBook("author2", "title6", "callNo6");
-		book[6]  = bookDAO.addBook("author2", "title7", "callNo7");
-		book[7]  = bookDAO.addBook("author2", "title8", "callNo8");
-		book[8]  = bookDAO.addBook("author3", "title9", "callNo9");
-		book[9]  = bookDAO.addBook("author3", "title10", "callNo10");
-		book[10] = bookDAO.addBook("author4", "title11", "callNo11");
-		book[11] = bookDAO.addBook("author4", "title12", "callNo12");
-		book[12] = bookDAO.addBook("author5", "title13", "callNo13");
-		book[13] = bookDAO.addBook("author5", "title14", "callNo14");
-		book[14] = bookDAO.addBook("author5", "title15", "callNo15");
-
+		//Add a book
+		IBook book  = bookDAO.addBook("author1", "title1", "callNo1");
+		//Add a member
 		IMember member = memberDAO.addMember("fName0", "lName0", "0001", "email0");
 		
-		try{
-			for (int i=0; i<6; i++) {
-				ILoan loan = loanDAO.createLoan(member, book[i]);
-				loanDAO.commitLoan(loan);
-			}
+		//There should be no loans for a new book.
+		assertNull(book.getLoan());
+		//Borrow the book
+		ILoan loan = loanDAO.createLoan(member, book);
 		
-			fail("Cannot borrow more than 6");
-		}catch(RuntimeException ignoredException){
-			
-		}
+		loanDAO.commitLoan(loan);
 		
-	}
+		//getLoan should be not null for a borrowed book
+		assertNotNull(book.getLoan());
 
+	}
+	
 
 }
