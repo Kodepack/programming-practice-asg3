c2b2a5ed7912ba15236317537905d1d2ca0d10e0
Andrew Perera <andrew5core@hotmail.com>
2016-09-21 13:33:13 +1000

    Memeber creation unit Test

--- ITC515_NewLibrary/src/test/library/daos/MemberHelperTest.java
+++ ITC515_NewLibrary/src/test/library/daos/MemberHelperTest.java
@@ -1,6 +1,9 @@
 package test.library.daos;
 
 import junit.framework.TestCase;
+import library.daos.MemberHelper;
+import library.interfaces.daos.IMemberHelper;
+import library.interfaces.entities.IMember;
 
 import org.junit.Test;
 
@@ -12,4 +15,21 @@ public class MemberHelperTest extends TestCase{
 	private static final String CONTACTPHONE="0452535065";
 	private static final String EMAIL="abc@gmail.com"; 
 	private static final int MEMBERID=54;
+	
+	@Test
+	
+	public void testmakeMember(){
+		
+		
+		IMemberHelper MemberHelper =new MemberHelper();
+		IMember member = MemberHelper.makeMember(FIRSTNAME, LASTNAME, CONTACTPHONE, EMAIL, MEMBERID);
+		assertEquals(member.getContactPhone(), CONTACTPHONE);
+		assertEquals(member.getFirstName(), FIRSTNAME);
+		assertEquals(member.getLastName(), LASTNAME);
+		assertEquals(member.getEmailAddress(), EMAIL);
+		assertEquals(member.getID(), MEMBERID);
+		
+
+	}
+	
 }
