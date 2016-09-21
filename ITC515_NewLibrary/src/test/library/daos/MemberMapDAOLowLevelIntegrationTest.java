953d9236e099f6c8130c506bd1a3a63c66b7c82b
Kishantha <kishantha@gmail.com>
2016-09-21 23:23:08 +1000

    Test for find members by email address

--- ITC515_NewLibrary/src/test/library/daos/MemberMapDAOLowLevelIntegrationTest.java
+++ ITC515_NewLibrary/src/test/library/daos/MemberMapDAOLowLevelIntegrationTest.java
@@ -114,5 +114,30 @@ public class MemberMapDAOLowLevelIntegrationTest {
 		assertEquals(MEMBER_01_CONTACT_EMAIL, member.getEmailAddress());
 		
 	}
+	
+	/**
+	 * Test for find members by email address
+	 */
+	@Test
+	public void testFindMembersByEmailAddress(){
+		IMemberDAO memberDAO = new MemberMapDAO(new MemberHelper());
+		//Add one member fist
+		memberDAO.addMember(MEMBER_01_FIRST_NAME, MEMBER_01_LAST_NAME, MEMBER_01_CONTACTPHONE_NUMBER, MEMBER_01_CONTACT_EMAIL);
+		
+		//Lets try to find member by last name
+		List<IMember> memberList = memberDAO.findMembersByEmailAddress(MEMBER_01_CONTACT_EMAIL);
+		IMember member = memberList.get(0);
+
+		//Lets check the size of the member list
+		assertEquals(1,memberList.size());
+		//lets check the member's properties
+		assertEquals(MEMBER_01_ID, member.getID());
+		assertEquals(MEMBER_01_FIRST_NAME, member.getFirstName());
+		assertEquals(MEMBER_01_LAST_NAME, member.getLastName());
+		assertEquals(MEMBER_01_CONTACTPHONE_NUMBER, member.getContactPhone());
+		assertEquals(MEMBER_01_CONTACT_EMAIL, member.getEmailAddress());
+		
+	}	
+	
 
 }
