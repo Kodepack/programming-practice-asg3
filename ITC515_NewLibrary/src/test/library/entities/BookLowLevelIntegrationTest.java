aa5a15bb3ddc2eb94e03ba53121536c3b32aee02
Kishantha <kishantha@gmail.com>
2016-09-21 21:23:20 +1000

    Test case for return book damaged and re borrow

--- ITC515_NewLibrary/src/test/library/entities/BookLowLevelIntegrationTest.java
+++ ITC515_NewLibrary/src/test/library/entities/BookLowLevelIntegrationTest.java
@@ -174,5 +174,200 @@ public class BookLowLevelIntegrationTest {
 	}
 	
 
+	/**
+	 * Test case for return book damaged and re borrow
+	 */
+	@Test
+	public void testReturnBorrowedDamagedBookAndReborrow(){
+		
+		IBookDAO bookDAO = new BookMapDAO(new BookHelper());
+		ILoanDAO loanDAO = new LoanMapDAO(new LoanHelper());
+		IMemberDAO memberDAO = new MemberMapDAO(new MemberHelper());
+
+		IMember member1 = memberDAO.addMember("fName0", "lName0", "0001", "email0");
+		IMember member2 = memberDAO.addMember("fName0", "lName0", "0001", "email0");
+		
+		IBook book  = bookDAO.addBook("author1", "title1", "callNo1");
+		final boolean DAMAGED = true;
+
+		//borrow the book by member1
+		ILoan loan1 = loanDAO.createLoan(member1, book);
+		loanDAO.commitLoan(loan1);
+		
+		//return the book damaged
+		book.returnBook(DAMAGED);
+		
+		try {
+			//borrow the book again by member2
+			ILoan loan2 = loanDAO.createLoan(member2, book);
+			loanDAO.commitLoan(loan2);
+			//this should fail
+			fail("cannot borrow a damaged book");
+		} catch (RuntimeException e) {
+			//ignore
+		}
+		assertEquals(EBookState.DAMAGED,book.getState());
+	}
+	
+	
+	
+	@Test
+	public void testLoseNewBook(){
+		
+		IBookDAO bookDAO = new BookMapDAO(new BookHelper());
+		
+		IBook book  = bookDAO.addBook("author1", "title1", "callNo1");
+
+		try {
+			//lose a new book
+			book.lose();
+			//this should fail
+			fail("cannot loose a new book");
+		} catch (RuntimeException e) {
+			//ignore
+		}
+		assertEquals(EBookState.AVAILABLE,book.getState());
+	}
+	
+	@Test
+	public void testLose(){
+		IBookDAO bookDAO = new BookMapDAO(new BookHelper());
+		ILoanDAO loanDAO = new LoanMapDAO(new LoanHelper());
+		IMemberDAO memberDAO = new MemberMapDAO(new MemberHelper());
+		
+		IBook book  = bookDAO.addBook("author1", "title1", "callNo1");
+		IMember member = memberDAO.addMember("fName0", "lName0", "0001", "email0");
+		//borrow the book
+		ILoan loan = loanDAO.createLoan(member, book);
+		loanDAO.commitLoan(loan);
+		//lose the book
+		book.lose();
+		//this should be successful
+		assertEquals(EBookState.LOST,book.getState());
+
+	}
+	
+	@Test
+	public void testRepairNewBook(){
+		
+		IBookDAO bookDAO = new BookMapDAO(new BookHelper());
+		
+		IBook book  = bookDAO.addBook("author1", "title1", "callNo1");
+		try {
+			//lose a new book
+			book.repair();
+			//this should fail
+			fail("cannot repair a new book");
+		} catch (RuntimeException e) {
+			//ignore
+		}
+		assertEquals(EBookState.AVAILABLE,book.getState());
+	}
+	
+	@Test
+	public void testRepairDamagedBook(){
+		
+		IBookDAO bookDAO = new BookMapDAO(new BookHelper());
+		ILoanDAO loanDAO = new LoanMapDAO(new LoanHelper());
+		IMemberDAO memberDAO = new MemberMapDAO(new MemberHelper());
+		
+		IBook book  = bookDAO.addBook("author1", "title1", "callNo1");
+		IMember member = memberDAO.addMember("fName0", "lName0", "0001", "email0");
+		
+		final boolean DAMAGED = true;
+		assertEquals(EBookState.AVAILABLE,book.getState());
+		//borrow the book
+		ILoan loan = loanDAO.createLoan(member, book);
+		loanDAO.commitLoan(loan);
+		assertEquals(EBookState.ON_LOAN,book.getState());
+
+		book.returnBook(DAMAGED);
+		assertEquals(EBookState.DAMAGED,book.getState());
+		book.repair();
+		assertEquals(EBookState.AVAILABLE,book.getState());
+		
+
+	}
+
+	@Test
+	public void testDisposeBorrowedBook(){
+		
+		IBookDAO bookDAO = new BookMapDAO(new BookHelper());
+		ILoanDAO loanDAO = new LoanMapDAO(new LoanHelper());
+		IMemberDAO memberDAO = new MemberMapDAO(new MemberHelper());
+		
+		IBook book  = bookDAO.addBook("author1", "title1", "callNo1");
+		IMember member = memberDAO.addMember("fName0", "lName0", "0001", "email0");
+		try {
+			ILoan loan = loanDAO.createLoan(member, book);
+			loanDAO.commitLoan(loan);
+			//lose a new book
+			book.dispose();
+			//this should fail
+			fail("cannot dispose a borrowed");
+		} catch (RuntimeException e) {
+			//ignore
+		}
+		assertEquals(EBookState.ON_LOAN,book.getState());
+	}
+	
+	
+	@Test
+	public void testDisposeNewBook(){
+		
+		IBookDAO bookDAO = new BookMapDAO(new BookHelper());
+		IBook book  = bookDAO.addBook("author1", "title1", "callNo1");
+		assertEquals(EBookState.AVAILABLE,book.getState());
+		//borrow the book
+		book.dispose();
+		assertEquals(EBookState.DISPOSED,book.getState());
+
+	}
+	
+	
+	@Test
+	public void testDisposeLostBook(){
+		
+		IBookDAO bookDAO = new BookMapDAO(new BookHelper());
+		ILoanDAO loanDAO = new LoanMapDAO(new LoanHelper());
+		IMemberDAO memberDAO = new MemberMapDAO(new MemberHelper());
+		
+		IBook book  = bookDAO.addBook("author1", "title1", "callNo1");
+		IMember member = memberDAO.addMember("fName0", "lName0", "0001", "email0");
+		assertEquals(EBookState.AVAILABLE,book.getState());
+		//borrow the book
+		ILoan loan = loanDAO.createLoan(member, book);
+		loanDAO.commitLoan(loan);
+		assertEquals(EBookState.ON_LOAN,book.getState());
+		book.lose();
+		assertEquals(EBookState.LOST,book.getState());
+		book.dispose();
+		assertEquals(EBookState.DISPOSED,book.getState());
+	}
+	
+	
+	@Test
+	public void testDisposeDamagedBook(){
+		
+		IBookDAO bookDAO = new BookMapDAO(new BookHelper());
+		ILoanDAO loanDAO = new LoanMapDAO(new LoanHelper());
+		IMemberDAO memberDAO = new MemberMapDAO(new MemberHelper());
+		
+		IBook book  = bookDAO.addBook("author1", "title1", "callNo1");
+		IMember member = memberDAO.addMember("fName0", "lName0", "0001", "email0");
+		
+		final boolean DAMAGED = true;
+
+		assertEquals(EBookState.AVAILABLE,book.getState());
+		//borrow the book
+		ILoan loan = loanDAO.createLoan(member, book);
+		loanDAO.commitLoan(loan);
+		assertEquals(EBookState.ON_LOAN,book.getState());
+		book.returnBook(DAMAGED);
+		assertEquals(EBookState.DAMAGED,book.getState());
+		book.dispose();
+		assertEquals(EBookState.DISPOSED,book.getState());
+	}	
+	
 	
 }
