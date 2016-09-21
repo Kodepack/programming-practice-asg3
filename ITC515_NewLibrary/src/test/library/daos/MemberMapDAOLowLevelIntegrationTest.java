f34ba5a7ce3cf70ecf05d8817a2ceee8eddebcf4
Kishantha <kishantha@gmail.com>
2016-09-21 23:20:34 +1000

    Test for get member by id

--- ITC515_NewLibrary/src/test/library/daos/MemberMapDAOLowLevelIntegrationTest.java
+++ ITC515_NewLibrary/src/test/library/daos/MemberMapDAOLowLevelIntegrationTest.java
@@ -45,5 +45,24 @@ public class MemberMapDAOLowLevelIntegrationTest {
 		
 	}
 
+	/**
+	 * Test for get member by id
+	 */
+	@Test
+	public void testGetMemberById(){
+		IMemberDAO memberDAO = new MemberMapDAO(new MemberHelper());
+		//Lets add member
+		memberDAO.addMember(MEMBER_01_FIRST_NAME, MEMBER_01_LAST_NAME, MEMBER_01_CONTACTPHONE_NUMBER, MEMBER_01_CONTACT_EMAIL);
+		//Lets try to get that member
+		IMember member = memberDAO.getMemberByID(MEMBER_01_ID);
+		//Lets check the members properties
+		assertEquals(MEMBER_01_ID, member.getID());
+		assertEquals(MEMBER_01_FIRST_NAME, member.getFirstName());
+		assertEquals(MEMBER_01_LAST_NAME, member.getLastName());
+		assertEquals(MEMBER_01_CONTACTPHONE_NUMBER, member.getContactPhone());
+		assertEquals(MEMBER_01_CONTACT_EMAIL, member.getEmailAddress());
+		
+	}
+
 
 }
