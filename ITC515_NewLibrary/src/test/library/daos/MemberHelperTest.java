0997c44ba5a15fc23d621bc10b5bf732a17baeb0
Andrew Perera <andrew5core@hotmail.com>
2016-09-21 11:37:34 +1000

    Import Libarires and adding constrains/ extend the class

--- ITC515_NewLibrary/src/test/library/daos/MemberHelperTest.java
+++ ITC515_NewLibrary/src/test/library/daos/MemberHelperTest.java
@@ -1,5 +1,15 @@
 package test.library.daos;
 
-public class MemberHelperTest {
+import junit.framework.TestCase;
 
+import org.junit.Test;
+
+
+public class MemberHelperTest extends TestCase{
+
+	private static final String FIRSTNAME="FIRSTNAME";
+	private static final String LASTNAME="LASTNAME";
+	private static final String CONTACTPHONE="0452535065";
+	private static final String EMAIL="abc@gmail.com"; 
+	private static final int MEMBERID=54;
 }
