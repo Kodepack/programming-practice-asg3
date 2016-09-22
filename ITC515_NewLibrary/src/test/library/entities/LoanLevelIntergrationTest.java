49b60699d7e0b3630a1d48c385c4ab9094c9b831
Andrew Perera <andrew5core@hotmail.com>
2016-09-22 14:34:45 +1000

    Class added for Intergration Loan Test

---
+++ ITC515_NewLibrary/src/test/library/entities/LoanLevelIntergrationTest.java
@@ -0,0 +1,21 @@
+package test.library.entities;
+
+import library.daos.BookHelper;
+import library.daos.BookMapDAO;
+import library.daos.LoanHelper;
+import library.daos.LoanMapDAO;
+import library.daos.MemberHelper;
+import library.daos.MemberMapDAO;
+import library.interfaces.daos.IBookDAO;
+import library.interfaces.daos.ILoanDAO;
+import library.interfaces.daos.IMemberDAO;
+import library.interfaces.entities.IBook;
+import library.interfaces.entities.IMember;
+
+public class LoanLevelIntergrationTest {
+	
+	
+	
+
+
+}
