411b771bbcd81643c644e74cde88cbca273a7cd3
Prabath <Prabathdee@yahoo.com>
2016-09-23 19:18:05 +1000

    test create loan

---
+++ ITC515_NewLibrary/src/test/library/daos/LoanMapDAOLowLevelIntegrationTest.java
@@ -0,0 +1,55 @@
+package test.library.daos;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Date;
+
+import library.daos.BookHelper;
+import library.daos.BookMapDAO;
+import library.daos.LoanHelper;
+import library.daos.LoanMapDAO;
+import library.daos.MemberHelper;
+import library.daos.MemberMapDAO;
+import library.interfaces.daos.IBookDAO;
+import library.interfaces.daos.ILoanDAO;
+import library.interfaces.daos.ILoanHelper;
+import library.interfaces.daos.IMemberDAO;
+import library.interfaces.entities.IBook;
+import library.interfaces.entities.ILoan;
+import library.interfaces.entities.IMember;
+
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Matchers;
+import org.mockito.Mockito;
+
+public class LoanMapDAOLowLevelIntegrationTest {
+
+	/**
+	 * Test create loan
+	 */
+	@Test
+	public void testCreateLoan(){
+		
+		IBookDAO bookDAO = new BookMapDAO(new BookHelper());
+		ILoanDAO loanDAO = new LoanMapDAO(new LoanHelper());
+		IMemberDAO memberDAO = new MemberMapDAO(new MemberHelper());
+		
+		IBook book  = bookDAO.addBook("author1", "title1", "callNo1");
+		IMember member = memberDAO.addMember("fName0", "lName0", "0001", "email0");
+		
+		ILoan loan = loanDAO.createLoan(member, book);
+		loanDAO.commitLoan(loan);
+
+		assertEquals(book,loan.getBook());
+		assertEquals(member,loan.getBorrower());
+
+		
+	}
+	
+
+	
+
+
+	
+}
