6fd314fe5af0c8ad7020f1382d5511d2ca6fdfd4
Kishantha <kishantha@gmail.com>
2016-09-23 17:50:53 +1000

    Test card swipe and book scan for borrower with no restrictions

--- ITC515_NewLibrary/src/test/SystemTest.java
+++ ITC515_NewLibrary/src/test/SystemTest.java
@@ -84,7 +84,7 @@ public class SystemTest {
 
     /**
      * Test card swipe and book scan for borrower with no restrictions
-     */
+     */ 
     @Test
     public void testCardSwipedAndBookScanForNoRestrictions() {
         //arrange
